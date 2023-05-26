dfadd1d124e1bcfece76b4baa9c59d304854cb96
maxOSfushta <fushta.m1997333@gmail.com>
2023-05-26 15:11:55 +0200

    added logic for saving image in gallery

--- app/src/main/java/com/example/top_publication_app/RedditPostViewHolder.java
+++ app/src/main/java/com/example/top_publication_app/RedditPostViewHolder.java
@@ -1,17 +1,29 @@
 package com.example.top_publication_app;
 
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 
 public class RedditPostViewHolder extends RecyclerView.ViewHolder {
@@ -35,6 +47,17 @@ public class RedditPostViewHolder extends RecyclerView.ViewHolder {
                 String originalUrl = post.getImageUrl();
                 openOriginalUrl(originalUrl);
             }
+        });
+
+        thumbnailImageView.setOnLongClickListener(v -> {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION) {
+                RedditPost post = posts.get(position);
+                String imageUrl = post.getImageUrl();
+                saveImageToGallery(itemView.getContext(), imageUrl);
+                return true;
+            }
+            return false;
         });
     }
 
@@ -60,6 +83,48 @@ public class RedditPostViewHolder extends RecyclerView.ViewHolder {
         itemView.getContext().startActivity(intent);
     }
 
+    private void saveImageToGallery(Context context, String imageUrl) {
+        Picasso.get().load(imageUrl).into(new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                ContentResolver resolver = context.getContentResolver();
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, "image_" + System.currentTimeMillis());
+                contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                    contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES);
+                } else {
+                    File imageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                    contentValues.put(MediaStore.Images.Media.DATA, imageDirectory.getAbsolutePath());
+                }
+
+                Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+                try {
+                    OutputStream outputStream = resolver.openOutputStream(imageUri);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                    outputStream.close();
+
+                    Toast.makeText(context, "Image saved to Gallery", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(context, "Failed to save image", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+                Toast.makeText(context, "Failed to load image", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        });
+    }
+
     private String getFormattedTimeAgo(long createdUtc) {
         long currentTime = System.currentTimeMillis() / 1000L;
         long timeDifference = currentTime - createdUtc;
@@ -71,10 +136,18 @@ public class RedditPostViewHolder extends RecyclerView.ViewHolder {
             return minutes + " minutes ago";
         } else if (timeDifference < 86400) {
             long hours = timeDifference / 3600;
-            return hours + " hours ago";
+            if (hours == 1) {
+                return "1 hour ago";
+            } else {
+                return hours + " hours ago";
+            }
         } else {
             long days = timeDifference / 86400;
-            return days + " days ago";
+            if (days == 1) {
+                return "1 day ago";
+            } else {
+                return days + " days ago";
+            }
         }
     }
 }
